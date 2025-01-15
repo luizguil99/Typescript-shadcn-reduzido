bdb5671024bafd0e0e618e4c1c26d705d64d6156
Guilherme <luiz.guilherme100@outlook.com>
2025-01-15 13:19:22 -0300

    stack-prismium

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { CrmSidebar } from "@/components/CrmSidebar";
+
+const Index = () => {
+  return (
+    <h1></h1>
+  );
+};
+
+export default Index;
